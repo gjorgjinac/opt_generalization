183669b86259b808eb2bbc09a22e682960bb5cf2
gjorgjina <gjorgjinac@gmail.com>
2023-12-01 12:47:57 +0000

    update shell scripts

--- S0_sample.sh
+++ S0_sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dimension=5
+dimension=10
 tsp python N0_sample_problems.py --dimension $dimension --random_problem_count 10000 --m4_problem_count 10000
--- S1_run_algorithms.sh
+++ S1_run_algorithms.sh
@@ -3,20 +3,13 @@
 dimension=10
 
 
-for benchmark in bbob affine
+for benchmark in bbob affine m4 random
 do
     for seed in {100..1001..100}
     do
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO1 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO2 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO3 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO4 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO8 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO10 --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms DE --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms GA --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms ES --benchmark $benchmark
-        #tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO --benchmark $benchmark
-        tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms CMAES --benchmark $benchmark
+        tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms DE --benchmark $benchmark
+        tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms GA --benchmark $benchmark
+        tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms ES --benchmark $benchmark
+        tsp python N1_run_algorithms.py --dimension $dimension --seed $seed --algorithms PSO --benchmark $benchmark
     done
 done
